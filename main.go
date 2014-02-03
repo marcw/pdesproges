71b10ded3f7748e85e2065a7ffde5172e72cebd7
Marc Weistroff <marc@weistroff.net>
2014-02-03 17:12:41 +0100

    Enable tweets

--- main.go
+++ main.go
@@ -13,17 +13,17 @@ import (
 func main() {
 	anaconda.SetConsumerKey(os.Getenv("TWITTER_CONSUMER_KEY"))
 	anaconda.SetConsumerSecret(os.Getenv("TWITTER_CONSUMER_SECRET"))
-	//api := anaconda.NewTwitterApi(os.Getenv("ACCESS_TOKEN"), os.Getenv("TOKEN_SECRET"))
+	api := anaconda.NewTwitterApi(os.Getenv("TWITTER_ACCESS_TOKEN"), os.Getenv("TWITTER_TOKEN_SECRET"))
 
 	rand.Seed(time.Now().UnixNano())
 	content, err := ioutil.ReadFile("desprogeades.txt")
-    if err != nil {
+	if err != nil {
 		log.Fatalln(err)
 	}
 	lines := strings.Split(string(content), "\n")
-
 	line := lines[rand.Intn(len(lines))]
 
-	//api.PostTweet(line, nil)
-    log.Println(line)
+	if _, err := api.PostTweet(line, nil); err != nil {
+		log.Fatalln(err)
+	}
 }
